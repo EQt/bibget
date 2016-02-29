9a2439e1477ea87eba8b361ba7908b9495049c15
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-02-29 15:21:20 +0100

    added existing files

---
+++ .bibrc.py
@@ -0,0 +1,10 @@
+import os
+
+BIBFILE  = './bibliography.bib'
+PDF_DIR  = 'literatur/'
+KEYWIDTH = 50
+
+# change working directory to main git directory
+main_dir = os.path.dirname(__file__)
+if len(main_dir) > 0: os.chdir(main_dir)
+
