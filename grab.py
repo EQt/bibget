8bcac855bf5a2f6df67ead34b3bf44d814724415
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-03-04 14:07:26 +0100

    python2 compatible

---
+++ grab.py
@@ -0,0 +1,44 @@
+from lxml import etree
+from io import StringIO
+import subprocess as sp
+
+try:
+    from urllib.request import urlopen
+    def readurl(url):
+        return urlopen(url).readall()
+except ImportError:
+    from urllib2 import urlopen
+    def readurl(url):
+        return urlopen(url).read()
+
+try:
+    from subprocess import DEVNULL  # py3k
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+try:
+    from urllib3.request import urlencode  # py3k
+except ImportError:
+    from urllib import urlencode
+
+def tidy_html(html):
+    """
+    Although there is a lib for it (https://pypi.python.org/pypi/pytidylib),
+    I prefer to call tidy myself.
+    """
+    html = html.replace(b"&nbsp;", b" ")
+    opts = {"stdout": sp.PIPE,
+            "stderr": DEVNULL,
+            "stdin": sp.PIPE}
+    with sp.Popen("tidy -asxhtml -utf8 -q".split(), **opts) as proc:
+        out, err = proc.communicate(input=html)
+        return out
+
+def getxml(url):
+    html = readurl(url)
+    xml = tidy_html(html)
+    return etree.parse(BytesIO(xml))
+
+def findx(xml, path):
+    ns = {'x': 'http://www.w3.org/1999/xhtml'}
+    return xml.xpath(path, namespaces=ns)
