4edea6dea5ddeeaedc372961c6bd54f76f983f92
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-03-04 17:52:29 +0100

    unquote

--- grab.py
+++ grab.py
@@ -17,9 +17,9 @@ except ImportError:
     import os
     DEVNULL = open(os.devnull, 'wb')
 try:
-    from urllib.parse import urlencode, urlparse  # py3k
+    from urllib.parse import urlencode, unquote  # py3k
 except ImportError:
-    from urllib import urlencode, urlparse
+    from urllib import urlencode, unquote
 
 def tidy_html(html):
     """
