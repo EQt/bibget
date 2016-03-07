f2a9e6d275fb43c954130e681ffd1ad500872e9a
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-03-07 13:52:43 +0100

    move to bib

--- bib.py
+++ bib.py
@@ -4,4 +4,27 @@ Some utility functions...
 
 import io
 
+def dumps(entry):
+    """
+    Format bibtex entry as string
+    """
+    import textwrap
+
+    if not isinstance(entry, dict):
+        return dumps(entry.get_entry_list()[0])
+    islow = lambda s: s[0].islower()
+    labels = list(filter(islow, entry.keys()))
+    width = 3 + max(map(len, labels))
+    indent = "\n" + " " * (width + 4)
+    formatter = ",\n  %%-%ds %%s" % width
+    s = "@%s{%s" % (entry["ENTRYTYPE"], entry["ID"])
+    line = lambda l: l if "\n" in l else indent.join(textwrap.wrap(l, 80 - width))
+    form = lambda s: formatter % (s + " = ", '{%s}' % line(entry[s]))
+    entry['author'] = entry['author'].replace("\n", " ")
+    entry['author'] = entry['author'].replace("and ", "and" + indent)
+    s = s + form('title') + form('author')
+    labels = set(labels) - set(['title', 'author'])
+    for l in labels:
+        s = s + form(l)
+    return s + "\n}\n\n"
 
--- springer.py
+++ springer.py
@@ -7,8 +7,8 @@ from __future__ import print_function, with_statement
 import sys
 from grab import *
 from bibtexparser.bparser import BibTexParser
-import textwrap
 from grab import getxml
+from bib import *
 # from bibtexparser import dumps
 
 def tex2utf8(s):
@@ -41,26 +41,6 @@ def bibid(entry):
     else:
         bid = "".join(map(lastname, authors))
     entry['ID'] = utf82e(bid + ":" + str(entry["year"]))
-
-
-def dumps(entry):
-    if not isinstance(entry, dict):
-        return dumps(entry.get_entry_list()[0])
-    islow = lambda s: s[0].islower()
-    labels = list(filter(islow, entry.keys()))
-    width = 3 + max(map(len, labels))
-    indent = "\n" + " " * (width + 4)
-    formatter = ",\n  %%-%ds %%s" % width
-    s = "@%s{%s" % (entry["ENTRYTYPE"], entry["ID"])
-    line = lambda l: l if "\n" in l else indent.join(textwrap.wrap(l, 80 - width))
-    form = lambda s: formatter % (s + " = ", '{%s}' % line(entry[s]))
-    entry['author'] = entry['author'].replace("\n", " ")
-    entry['author'] = entry['author'].replace("and ", "and" + indent)
-    s = s + form('title') + form('author')
-    labels = set(labels) - set(['title', 'author'])
-    for l in labels:
-        s = s + form(l)
-    return s + "\n}\n\n"
 
 
 def bibentry(url):
